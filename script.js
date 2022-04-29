1370f23045cb865355b97b32f368b512d2e84b08
Alexandra Krasnova <krasnova@physics.msu.ru>
2022-04-29 14:10:03 +0300

    Added validation of entered values

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+'use strict';
+
+let numberOfFilms;
+
+do {
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+} while (numberOfFilms == null || numberOfFilms === "" ||  isNaN(numberOfFilms));
+
+
+
+if (numberOfFilms < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+    alert("Вы классический зритель");
+} else if (numberOfFilms > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -13,9 +31,13 @@ let rating;
 
 for (let i = 0; i < 2; i++) {
     movie = prompt("Один из просмотренных фильмов?", "");
-    rating = prompt("На склоько оцените его?", "");
+    rating = prompt("На сколько оцените его?", "");
 
-    personalMovieDB.movies[movie] = rating;
+    if (movie != null && rating != null && movie != "" && rating != "" && movie.length < 50) {
+        personalMovieDB.movies[movie] = rating;
+    } else {
+        i--;
+    }
 }
 
 console.log(personalMovieDB);
